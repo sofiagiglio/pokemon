af992a07ed3c3f7591cbc586adf1012dab58ef46
johana_sofi <johana.torrilla@sagradoalcal.edu.ar>
2023-09-25 17:18:54 -0300

    pokeapi_con_imagen

--- pokemon.py
+++ pokemon.py
@@ -1,32 +1,59 @@
-import tkinter as tk
-from json import tk
-from flask import Flask, request, jsonify
 import requests
+import tkinter as tk
+from tkinter import messagebox
+import io
+from PIL import Image, ImageTk
 
-app = Flask(__name__)
+def get_pokemon_info():
+    pokemon_name = entry.get().lower()
+    url = f'https://pokeapi.co/api/v2/pokemon/{pokemon_name}/'
 
-@app.route('/pokemon/<name_or_id>', methods=['GET'])
-def get_pokemon_info(name_or_id):
-
-    url = f'https://pokeapi.co/api/v2/pokemon/{name_or_id}/'
-    
     response = requests.get(url)
-    
+
     if response.status_code == 200:
         data = response.json()
-        pokemon_info = {
-            'name': data['name'],
-            'id': data['id'],
-            'height': data['height'],
-            'weight': data['weight'],
-            'types': [t['type']['name'] for t in data['types']],
-        }
-        return jsonify(pokemon_info)
+        pokemon_info = f"Nombre: {data['name'].capitalize()}\n" \
+                       f"ID: {data['id']}\n" \
+                       f"Altura: {data['height']}\n" \
+                       f"Peso: {data['weight']}\n" \
+                       "Tipos: " + ", ".join([t['type']['name'] for t in data['types']])
+        result_label.config(text=pokemon_info)
+
+        # Obtener la URL de la imagen del Pokémon
+        image_url = data['sprites']['front_default']
+
+        # Descargar la imagen y mostrarla en el Label de la imagen
+        response = requests.get(image_url)
+        img_data = response.content
+        img = Image.open(io.BytesIO(img_data))
+        img.thumbnail((300, 300))  # Ajusta el tamaño de la imagen
+        img = ImageTk.PhotoImage(img)
+        image_label.config(image=img)
+        image_label.image = img
     else:
-        return jsonify({'error': 'Pokemon no encontrado'}), 404
+        result_label.config(text="Pokemon no encontrado")
+        messagebox.showerror("Error", "Pokemon no encontrado")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# Crear la ventana de la aplicación
+app = tk.Tk()
+app.title("¿Quién es ese Pokémon?")
 
+# Crear elementos de la interfaz gráfica
+label = tk.Label(app, text="Ingresa el nombre o ID del Pokémon:")
+label.pack()
 
+entry = tk.Entry(app)
+entry.pack()
 
+search_button = tk.Button(app, text="Buscar", command=get_pokemon_info)
+search_button.pack()
+
+result_label = tk.Label(app, text="")
+result_label.pack()
+
+# Crear un Label para la imagen del Pokémon
+image_label = tk.Label(app)
+image_label.pack()
+
+app.geometry("")
+app.mainloop()
