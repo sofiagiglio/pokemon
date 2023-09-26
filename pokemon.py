13cb1526418fceddaad5efed5cff396e2e71cf65
johana_sofi <johana.torrilla@sagradoalcal.edu.ar>
2023-09-26 17:15:01 -0300

    quien_pokemon

--- pokemon.py
+++ pokemon.py
@@ -3,9 +3,59 @@ import tkinter as tk
 from tkinter import messagebox
 import io
 from PIL import Image, ImageTk
+import random
 
-def get_pokemon_info():
-    pokemon_name = entry.get().lower()
+def get_pokemon_list():
+    url = 'https://pokeapi.co/api/v2/pokemon?limit=1000'  # Obtén una lista de 1000 Pokémon
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        pokemon_names = [pokemon['name'] for pokemon in data['results']]
+        return pokemon_names
+    else:
+        messagebox.showerror("Error", "No se pudo obtener la lista de Pokémon")
+        return []
+
+# Variable para realizar un seguimiento del Pokémon actual
+current_pokemon = None
+
+def get_random_pokemon():
+    global current_pokemon
+    # Borra la información del Pokémon anterior
+    result_label.config(text="")
+
+    # Obtener un Pokémon aleatorio de la lista
+    current_pokemon = random.choice(pokemon_list)
+    url = f'https://pokeapi.co/api/v2/pokemon/{current_pokemon}/'
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        # Obtener la URL de la imagen del Pokémon
+        image_url = data['sprites']['front_default']
+
+        # Descargar la imagen y mostrarla en el Label de la imagen
+        response = requests.get(image_url)
+        img_data = response.content
+        img = Image.open(io.BytesIO(img_data))
+        img = ImageTk.PhotoImage(img, width=100, height=70)
+        image_label.config(image=img, compound=tk.CENTER)
+        image_label.image = img
+
+def check_guess():
+    if current_pokemon is not None:
+        guess = entry.get().lower()
+        if guess == current_pokemon:
+            # Mostrar la información del Pokémon en el Label de resultado
+            display_pokemon_info(current_pokemon)
+        else:
+            messagebox.showerror("Incorrecto", f"Inténtalo de nuevo. El Pokémon no es {guess.capitalize()}.")
+
+def display_pokemon_info(pokemon_name):
     url = f'https://pokeapi.co/api/v2/pokemon/{pokemon_name}/'
 
     response = requests.get(url)
@@ -18,42 +68,36 @@ def get_pokemon_info():
                        f"Peso: {data['weight']}\n" \
                        "Tipos: " + ", ".join([t['type']['name'] for t in data['types']])
         result_label.config(text=pokemon_info)
-
-        # Obtener la URL de la imagen del Pokémon
-        image_url = data['sprites']['front_default']
-
-        # Descargar la imagen y mostrarla en el Label de la imagen
-        response = requests.get(image_url)
-        img_data = response.content
-        img = Image.open(io.BytesIO(img_data))
-        img.thumbnail((300, 300))  # Ajusta el tamaño de la imagen
-        img = ImageTk.PhotoImage(img)
-        image_label.config(image=img)
-        image_label.image = img
     else:
-        result_label.config(text="Pokemon no encontrado")
-        messagebox.showerror("Error", "Pokemon no encontrado")
+        result_label.config(text="Información del Pokémon no disponible")
+
+# Obtener la lista de nombres de Pokémon desde la API
+pokemon_list = get_pokemon_list()
 
 # Crear la ventana de la aplicación
 app = tk.Tk()
-app.title("¿Quién es ese Pokémon?")
+app.title("Adivina el Pokémon")
 
 # Crear elementos de la interfaz gráfica
-label = tk.Label(app, text="Ingresa el nombre o ID del Pokémon:")
+label = tk.Label(app, text="¿Quién es ese Pokémon?:")
 label.pack()
 
 entry = tk.Entry(app)
 entry.pack()
 
-search_button = tk.Button(app, text="Buscar", command=get_pokemon_info)
-search_button.pack()
+check_button = tk.Button(app, text="Comprobar", command=check_guess)
+check_button.pack()
 
-result_label = tk.Label(app, text="")
-result_label.pack()
+get_random_pokemon_button = tk.Button(app, text="Siguiente Pokémon", command=get_random_pokemon)
+get_random_pokemon_button.pack()
 
 # Crear un Label para la imagen del Pokémon
 image_label = tk.Label(app)
 image_label.pack()
 
-app.geometry("")
+# Crear un Label para mostrar la información del Pokémon adivinado
+result_label = tk.Label(app, text="")
+result_label.pack()
+
+app.geometry("400x400")  # Tamaño inicial de la ventana
 app.mainloop()
